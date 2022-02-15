56e670e488f43dec5d76bf708a1e6a772769e5b5
GlennMiller1991 <gatesoftommorow91@mail.ru>
2022-02-15 14:14:35 +0300

    skill was corrected

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -6,39 +6,73 @@ import {faJs} from "@fortawesome/free-brands-svg-icons/faJs";
 import {faFileCode, faProjectDiagram} from "@fortawesome/free-solid-svg-icons";
 import {faCss3Alt} from "@fortawesome/free-brands-svg-icons/faCss3Alt";
 import {faHtml5, faSass} from "@fortawesome/free-brands-svg-icons";
-import {faNetworkWired} from "@fortawesome/free-solid-svg-icons/faNetworkWired";
 import {faVials} from "@fortawesome/free-solid-svg-icons/faVials";
-import {faBook} from "@fortawesome/free-solid-svg-icons/faBook";
 import {faGithubSquare} from "@fortawesome/free-brands-svg-icons/faGithubSquare";
 import {faHighlighter} from "@fortawesome/free-solid-svg-icons/faHighlighter";
 import {faEdit} from "@fortawesome/free-solid-svg-icons/faEdit";
 import {faTextHeight} from "@fortawesome/free-solid-svg-icons/faTextHeight";
 import {faPython} from "@fortawesome/free-brands-svg-icons/faPython";
-import {faFlask} from "@fortawesome/free-solid-svg-icons/faFlask";
 
 
 
 export const Skills = React.memo(() => {
     const skillsEntities = useMemo(() => {
         return [
-            {name:'JavaScript',icon:faJs,description:''},
-            {name:'TypeScript',icon:faTextHeight,description:''},
-            {name:'Thunk',icon:faFileCode,description:''},
-            {name:'CSS3',icon:faCss3Alt,description:''},
-            {name:'SASS',icon:faSass,description:''},
-            {name:'HTML',icon:faHtml5,description:''},
-            {name:'Axios',icon:faNetworkWired,description:''},
-            {name:'Story Book',icon:faBook,description:''},
-            {name:'Rest API',icon:faProjectDiagram,description:''},
-            {name:'Unit test',icon:faVials,description:''},
-            {name:'Git',icon:faGithubSquare,description:''},
-            {name:'Material UI',icon:faHighlighter,description:''},
-            {name:'Ant Design',icon:faEdit,description:''},
-            {name:'Python v3.6.x',icon:faPython,description:''},
-            {name:'Flask',icon:faFlask,description:''},
+            {
+                name:'JavaScript',
+                icon:faJs,
+                description:'Context, Closures, Event loop, Promises, Classes, Callbacks and more'
+            },
+            {
+                name:'TypeScript',
+                icon:faTextHeight,
+                description:'Types, Interfaces, Generics, Extends, Enums, Mixins, Utility types and more'},
+            {
+                name:'Thunk',
+                icon:faFileCode,
+                description:'Redux Middleware'},
+            {
+                name:'CSS3',
+                icon:faCss3Alt,
+                description:'Responsive Layout, Media queries and more'},
+            {
+                name:'SASS',
+                icon:faSass,
+                description:'Imports, Mixins, Variables, Functions, Nesting and more'},
+            {
+                name:'HTML',
+                icon:faHtml5,
+                description:'Semantic Layout, БЭМ'},
+            {
+                name:'Rest API',
+                icon:faProjectDiagram,
+                description:'Restful API, Axios, Fetch, Error Handling and more'},
+            {
+                name:'Python v3.6.x',
+                icon:faPython,
+                description:'SQLAlchemy, Flask and more'
+            },
+            {
+                name:'Unit test',
+                icon:faVials,
+                description:'TDD, StoryBook, Jest'},
+            {
+                name:'Git',
+                icon:faGithubSquare,
+                description:''},
+            {
+                name:'Material UI',
+                icon:faHighlighter,
+                description:''
+            },
+            {
+                name:'Ant Design',
+                icon:faEdit,
+                description:''
+            },
+
         ]
     }, [])
-    const fakeDescription = 'Dolores sit ipsum velit purus aliquet, massa fringilla leo the drana.'
     return (
         <div id={'skills'} className={styles.skills}>
             <div className={`${commonStyles.container} ${styles.container}`}>
@@ -49,7 +83,7 @@ export const Skills = React.memo(() => {
                 <div className={styles.skillsContainer}>
                     {
                         skillsEntities.map((skill, id) => {
-                            return <Skill key={id} {...skill} description={fakeDescription}/>
+                            return <Skill key={id} {...skill}/>
                         })
                     }
                 </div>
